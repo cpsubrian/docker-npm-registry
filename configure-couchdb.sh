4767647865370f8b1b3bb8866f26cd17f2863acb
Jan Willem van Diermen <jwvdiermen@gmail.com>
2014-01-07 13:02:40 +0100

    Add missing 'cd' command

--- configure-couchdb.sh
+++ configure-couchdb.sh
@@ -3,6 +3,7 @@
 couchdb -b
 sleep 5
 curl -X PUT http://localhost:5984/registry
+cd /tmp/npmjs.org
 couchapp push registry/app.js http://localhost:5984/registry
 couchapp push www/app.js http://localhost:5984/registry
 couchdb -d
